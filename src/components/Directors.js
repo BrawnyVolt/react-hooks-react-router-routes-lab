530558409a55166b79459943188e4f76cb0c4e09
Steven <shyfay0@gmail.com>
2021-10-01 10:22:13 -0700

    nailed it, turned our brains on

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const direction = directors.map(director => {
+    return(
+      <div key={directors.name}>
+        <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => {
+          return  <li key={movie}>{movie}</li>
+        })}
+      </ul>
+    </div>
+ )})
+ 
+  return (
+    <div>
+      <h1>Director's Page</h1>
+      {direction}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+
+ const movieStuff = movies.map(movie => {
+      return(
+        <div>
+          <h2>{movie.title}</h2>
+          <h3>Runtime: {movie.time} Minutes</h3>
+        <ul>
+          {movie.genres.map((genre) => {
+            return  <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+   )})
+   
+  return (
+    <div>
+    <h1>Movies Page</h1>
+    {movieStuff}
+    </div>
+  )
+
+  }
+
+
+// 
 
 export default Movies;
